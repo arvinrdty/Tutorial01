37a8ddef71c04b9eb13c474683c9b31619368efe
arvinrdty <arvinrdty@gmail.com>
2018-09-07 16:31:27 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,26 @@
+package coba;
+
+public class Mahasiswa {
+	private String coba;
+
+	
+	public static void main(String[]args){
+		System.out.println("hello world");
+		
+	}
+	
+	public String getCoba() {
+		return coba;
+	}
+
+
+
+
+	public void setCoba(String coba) {
+		this.coba = coba;
+	}
+
+
+	
+
+}
