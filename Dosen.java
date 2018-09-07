3c5c1279d7457db6db02d742ea87bf90aaa134ca
arvinrdty <arvinrdty@gmail.com>
2018-09-07 16:36:57 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,22 @@
+package coba;
+
+public class Dosen {
+	private String cobaLagi;
+
+	
+	public static void main(String[]args){
+		System.out.println("hello world");
+		
+	}
+	
+	public String getCoba() {
+		return cobaLagi;
+	}
+
+
+
+
+	public void setCoba(String coba) {
+		this.cobaLagi = coba;
+	}
+}
